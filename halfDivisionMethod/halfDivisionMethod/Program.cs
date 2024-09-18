96099353699554eb1276dc1d1869024b7dc99889
jecsonM <eugene.moiseiev@gmail.com>
2024-09-18 19:22:05 +0300

    Создал программу по численным методам для метода половинного деления

---
+++ halfDivisionMethod/halfDivisionMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static double F(double x)
+    {
+        return (2 * Math.Sin(x) - Math.Atan(x));
+    }
+    static void Main(string[] args)
+    {
+        double a, b,c, epsilon, x;
+
+        do
+        {
+            Console.Write("Введите а: ");
+        }
+        while (!double.TryParse(Console.ReadLine(), out a));
+
+        do
+        {
+            Console.Write("Введите b: ");
+        }
+        while (!double.TryParse(Console.ReadLine(), out b));
+
+        do
+        {
+            Console.Write("Введите epsilon: ");
+        }
+        while (!double.TryParse(Console.ReadLine(), out epsilon));
+
+        while (b - a > 2 * epsilon)
+        {
+            c = (a+b)/2;
+            if ( F(a)*F(c) < 0) b = c; else a = c;
+        }
+        x = (a + b) / 2;
+        Console.WriteLine($"Ноль в промежутке равен: {x} (x)");
+    }
+}
